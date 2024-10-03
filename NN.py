c81149992ad29c160919d50317c5b611f4bc6d5b
xuridi01 <xuridi01@vutbr.cz>
2024-10-03 11:46:29 +0200

    feat: implementation of feed forward neural network

---
+++ NN.py
@@ -0,0 +1,95 @@
+import  numpy as np
+
+def sigmoid_function(x):
+    return 1/(1+np.exp(-x))
+
+def sigmoid_derivative(x):
+    return sigmoid_function(x)*(1-sigmoid_function(x))
+
+class NeuralNetwork:
+    def __init__(self, layers):
+        self.layers = layers
+        self.num_layers = len(layers)
+        self.biases = [np.random.randn(x, 1) for x in self.layers[1:]]
+        self.weights = [np.random.randn(y, x) for x,y in zip(self.layers[:-1], self.layers[1:])]
+
+    def feed_forward(self, previous_activation):
+        act = previous_activation
+
+        #computing whole layer, dot - matrix of weights with vector of activation of previous layer
+        for w, b in zip(self.weights, self.biases):
+            act = sigmoid_function(np.dot(w, act) + b)
+
+        #returning vector of output layer activations
+        return act
+
+    def feed_forward_activations_and_z_arrays(self, previous_activation):
+        #arrays for returning activations and Zs
+        activations_arr = [previous_activation]
+        z_arr = []
+
+        #computing whole layer, dot - matrix of weights with vector of activation of previous layer
+        for w, b in zip(self.weights, self.biases):
+            z = np.dot(w, activations_arr[-1]) + b
+            z_arr.append(z)
+            layer_activation = sigmoid_function(z)
+            activations_arr.append(layer_activation)
+
+        return activations_arr, z_arr
+
+    @staticmethod
+    def compute_loss(y_true, y_pred):
+        return np.mean((y_true - y_pred) ** 2)
+
+
+    #computing gradients for every w and b, using 4 equations of backpropagation
+    def backpropagation(self, input_values, true_values):
+        #preparing arrays for gradients
+        w_gradients = [np.zeros(w.shape) for w in self.weights]
+        b_gradients = [np.zeros(b.shape) for b in self.biases]
+
+        #feed forward first to get activations and z from every layer
+        activations_arr, z_arr = self.feed_forward_activations_and_z_arrays(input_values)
+
+        #output layer error and gradients
+        error = (activations_arr[-1] - true_values) * sigmoid_derivative(z_arr[-1])
+        w_gradients[-1] = np.dot(error, activations_arr[-2].transpose())
+        b_gradients[-1] = error
+
+        #hidden layers
+        for i in range(2, self.num_layers):
+            error = (np.dot(self.weights[-i + 1].transpose(), error) * sigmoid_derivative(z_arr[-i]))
+            w_gradients[-i] = np.dot(error, activations_arr[-i - 1].transpose())
+            b_gradients[-i] = error
+
+        return w_gradients, b_gradients
+
+    def update_parameters(self, weight_gradients, bias_gradients, learning_rate):
+        self.weights = [w - learning_rate * dw for w, dw in zip(self.weights, weight_gradients)]
+        self.biases = [b - learning_rate * db for b, db in zip(self.biases, bias_gradients)]
+
+    def evaluate(self, t_data):
+        correct_predictions = 0
+        loss_sum = 0
+        for x, y in t_data:
+            output_activation = self.feed_forward(x)
+            #we need to handle categorical format of output and expected output ->argmax
+            correct_predictions += np.argmax(output_activation) == np.argmax(y)
+            loss_sum += self.compute_loss(y, output_activation)
+
+        return correct_predictions, loss_sum
+
+    def train_network(self, training_data, epochs, learning_rate, t_data):
+        n_test = len(t_data)
+
+        #training network through epochs
+        for i in range(epochs):
+            for input_data, expected_output in training_data:
+                w_g, b_g = self.backpropagation(input_data, expected_output)
+                self.update_parameters(w_g, b_g, learning_rate)
+
+            #evaluating correct predictions and loss on test data
+            corr, loss_sum = self.evaluate(t_data)
+            loss = loss_sum / n_test
+            print(f"Epoch {i+1}/{epochs} completed. Correctly classified {corr}/{n_test}. Accuracy: {100*corr/n_test} % ")
+            print(f"Epoch {i+1}, Loss: {loss}")
---
+++ main.py
@@ -0,0 +1,40 @@
+import numpy as np
+from sklearn.datasets import fetch_openml
+import matplotlib.pyplot as plt
+import NN
+
+# Loading the MNIST dataset
+mnist = fetch_openml('mnist_784', version=1)
+
+# Converting the data into NumPy arrays
+images = mnist.data.values
+labels = mnist.target.values.astype(int)
+
+#categorical -> got 10 neurons and neuron with the highest value is representing the prediction
+def sparse_to_categorical(label,num_classes):
+    res=np.zeros((num_classes,1))
+    res[label]=1
+    return res
+
+# def plot_mnist(data):
+#     """Plots 16 images from the data"""
+#     fig,axes=plt.subplots(4,4,figsize=(16,16))
+#     for i, ax in enumerate(axes.flatten()):
+#         ax.imshow(data[i][0].reshape((28,28)),cmap='gray')
+#         ax.set_title(f"Label {np.argmax(data[i][1])}")
+#     plt.show()
+
+training_data = [(x.reshape(784,1) / 255.0, sparse_to_categorical(y, 10)) for x, y in zip(images, labels)]
+
+test_data = training_data[-10000:]
+training_data = training_data[:-10000]
+
+#here we can experiment with params for learning
+epochs = 5
+learning_rate = 0.5
+
+#plot_mnist(training_data[:16])
+
+NN=NN.NeuralNetwork([784,20,10])
+NN.train_network(training_data, epochs, learning_rate, test_data)
+
